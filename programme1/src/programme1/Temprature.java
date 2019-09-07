5dcbd9a05c7451d6dd68a2d0872f34c12dfc323d
inder jeet singh <inder jeet singh@DESKTOP-UB66FQV>
2019-09-07 06:04:48 +0530

    first

--- programme1/src/programme1/Temprature.java
+++ programme1/src/programme1/Temprature.java
@@ -1,13 +1,14 @@
 package programme1;
 
 public class Temprature {
+	
 
 	
-	public static double convertToFarnheit(double celsius) {
+	public  double convertToFarnheit(double celsius) {
 		return celsius*9/5+32;
 		
 	}
-	public static double convertToCelsius(double farnheit) {
+	public  double convertToCelsius(double farnheit) {
 		return (farnheit-32)*5/9;
 	}
 
