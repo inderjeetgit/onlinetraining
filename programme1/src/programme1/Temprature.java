2a589dbc800ccbc8048a0d007e59f25a158e0259
inder jeet singh <inder jeet singh@DESKTOP-UB66FQV>
2019-09-07 06:23:58 +0530

    first

--- programme1/src/programme1/Temprature.java
+++ programme1/src/programme1/Temprature.java
@@ -2,7 +2,10 @@ package programme1;
 
 public class Temprature {
 	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+	}
 	
 	public  double convertToFarnheit(double celsius) {
 		return celsius*9/5+32;
