d0b06d7c0761a35aacbed7c77a67fb566eda7e08
inder jeet singh <inder jeet singh@DESKTOP-UB66FQV>
2019-09-07 06:26:20 +0530

    fifth

--- programme5/src/programme5/Factorial.java
+++ programme5/src/programme5/Factorial.java
@@ -4,10 +4,9 @@ public class Factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(calculateFactorial(5));
 
 	}
-	public static int calculateFactorial(int number) {
+	public  int calculateFactorial(int number) {
 		if(number==1)return 1;
 		else {
 			return number*calculateFactorial(number-1);
